cc5a0c2a2e68984abc2e84ecc06af387e9553dc3
Hugo Locurcio <hugo.locurcio@hugo.pro>
2022-07-23 20:05:49 +0200

    Make the FPS reported by `Engine.get_frames_per_second()` smoother
    This uses an averaging algorithm to smooth out the FPS counter.

--- main/main.cpp
+++ main/main.cpp
@@ -3170,7 +3170,13 @@ bool Main::iteration() {
 	frames++;
 	Engine::get_singleton()->_process_frames++;
 
-	if (frame > 1000000) {
+	// Smoothly update FPS over time, while ensuring the speed at which FPS stabilizes is not dependent on FPS.
+	// The FPS smooth factor is tuned to always *mostly* converge within a second.
+	constexpr int FPS_SMOOTH_FACTOR = 5;
+	const double ratio = FPS_SMOOTH_FACTOR * USEC_TO_SEC(ticks_elapsed);
+	Engine::get_singleton()->_fps = Engine::get_singleton()->_fps * (1 - ratio) + ratio * 1'000'000.0 / ticks_elapsed;
+
+	if (frame > 1'000'000) {
 		// Wait a few seconds before printing FPS, as FPS reporting just after the engine has started is inaccurate.
 		if (hide_print_fps_attempts == 0) {
 			if (editor || project_manager) {
@@ -3184,13 +3190,12 @@ bool Main::iteration() {
 			hide_print_fps_attempts--;
 		}
 
-		Engine::get_singleton()->_fps = frames;
 		performance->set_process_time(USEC_TO_SEC(process_max));
 		performance->set_physics_process_time(USEC_TO_SEC(physics_process_max));
 		process_max = 0;
 		physics_process_max = 0;
 
-		frame %= 1000000;
+		frame %= 1'000'000;
 		frames = 0;
 	}
 
