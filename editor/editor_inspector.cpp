f3a80652e07355565cb7b0710f41013ae95221f3
kobewi <kobewi4e@gmail.com>
2023-09-27 22:37:24 +0200

    Improve usage of String.split() vs get_slice()

--- editor/editor_inspector.cpp
+++ editor/editor_inspector.cpp
@@ -2768,10 +2768,9 @@ void EditorInspector::update_tree() {
 			// Setup a property sub-group.
 			subgroup = p.name;
 
-			Vector<String> hint_parts = p.hint_string.split(",");
-			subgroup_base = hint_parts[0];
-			if (hint_parts.size() > 1) {
-				section_depth = hint_parts[1].to_int();
+			subgroup_base = p.hint_string.get_slice(",", 0);
+			if (p.hint_string.get_slice_count(",") > 1) {
+				section_depth = p.hint_string.get_slice(",", 1).to_int();
 			} else {
 				section_depth = 0;
 			}
@@ -2782,10 +2781,9 @@ void EditorInspector::update_tree() {
 			// Setup a property group.
 			group = p.name;
 
-			Vector<String> hint_parts = p.hint_string.split(",");
-			group_base = hint_parts[0];
-			if (hint_parts.size() > 1) {
-				section_depth = hint_parts[1].to_int();
+			group_base = p.hint_string.get_slice(",", 0);
+			if (p.hint_string.get_slice_count(",") > 1) {
+				section_depth = p.hint_string.get_slice(",", 1).to_int();
 			} else {
 				section_depth = 0;
 			}
